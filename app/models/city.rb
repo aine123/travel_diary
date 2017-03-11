1d67aa907428657b04303eb6fb41088adfd7e5ec
Rails Girls Dub <serena.fritsch@gmail.com>
2017-03-11 12:23:30 +0000

    image uploads

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,7 @@
 class City < ActiveRecord::Base
-has_many :reviews
+    has_many :reviews
+    has_attached_file :picture, styles: { medium: "300x300#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
+
+    validates_attachment_content_type :picture, content_type: /\image\/.*\z/
+
 end
